826a751a67256946cad2a997698105fc77004a1b
piotrcieslik98 <piocie19981@gmail.com>
2024-11-01 18:13:17 +0100

    Zmiany 1.11.2024/2

---
+++ database/migrations/2024_11_01_165315_create_results_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable(); // jeśli chcesz przechowywać identyfikator użytkownika
+            $table->string('recommended_country');
+            $table->string('weather');
+            $table->string('recommended_activities');
+            $table->string('budget');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
